2de9cb95b0a064bfe488760ddc1fa2ba6b69ea01
Ryan Drew <drew.r@northeastern.edu>
2021-07-14 20:49:07 -0400

    Uh oh... stinky... 💩

---
+++ app.js
@@ -0,0 +1,22 @@
+const { App } = require('@slack/bolt');
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN
+});
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+console.log(message)
+  await say(`<@UM3U24PEY> just pooped his pants! OMG! 😱😱`);
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
